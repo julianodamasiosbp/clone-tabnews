91327bea47fb5c5ecfd7b12f94056845aa8640e3
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-10 22:30:35 +0000

    add `database` and `env` files

---
+++ infra/database.js
@@ -0,0 +1,25 @@
+import pg from 'pg';
+
+
+async function query(queryObject) {
+  const { Client } = pg;
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+  });
+  await client.connect();
+  
+  const res = await client.query(queryObject);
+  
+  await client.end();
+
+  return res;
+}
+
+export default {query: query};
+
+
+
