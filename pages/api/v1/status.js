82d6d28e27033c7d697be395e320b5a5aec957c5
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-11 19:23:55 +0000

    add services scripts

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,7 +1,8 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum')
+  const result = await database.query('SELECT 1 + 1 as sum');
+  console.log(result);
   response.status(200).json({ message: "Bem-vindo ao endpoint de Status" });
 }
 
