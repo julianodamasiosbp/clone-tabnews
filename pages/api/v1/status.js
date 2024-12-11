af6113598c0944b81817813612acc4ba3562ef64
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-11 14:53:15 +0000

    add `jsconfig.json` file

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,9 +1,7 @@
-import database from '../../../infra/database.js'
+import database from 'infra/database.js'
 
 async function status(request, response) {
   const result = await database.query('SELECT 1 + 1 as sum')
-  console.log('#######')
-  console.log(result)
   response.status(200).json({ message: "Bem-vindo ao endpoint de Status" });
 }
 
