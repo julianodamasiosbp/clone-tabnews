b991a45dede1a5bfd52c82c56aaeace4e730faa1
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-12 19:25:41 +0000

    finish endpoint `/api/v1/status`

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,10 +1,44 @@
-import database from 'infra/database.js'
+import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const versionDatabase = await getSQLVersion();
+  const maxDatabaseConnections = await getSQLMaxConnection();
+  const openedDatabaseConnections = await getSQLConnectionUsed();
   response.status(200).json({
-    updated_at: updatedAt
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionDatabase,
+        max_connections: maxDatabaseConnections,
+        opened_connections: openedDatabaseConnections,
+      },
+    },
   });
+}
+
+async function getSQLVersion() {
+  /*
+  const queryResult = await database.query("SELECT version();");
+  return queryResult.rows[0].version.toString().substring(0, 15);
+  */
+  const queryResult = await database.query("SHOW server_version;");
+  return queryResult.rows[0].server_version;
+}
+
+async function getSQLMaxConnection() {
+  const queryResult = await database.query("SHOW max_connections;");
+  return parseInt(queryResult.rows[0].max_connections);
+}
+
+async function getSQLConnectionUsed() {
+  const databaseName = process.env.POSTGRES_DB;
+  const queryResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  });
+  console.log(queryResult.rows)
+  return queryResult.rows[0].count;
 }
 
 export default status;
