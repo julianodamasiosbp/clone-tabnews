25391d79059a2188edcfcbd2053d3052d123d4e8
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-16 14:27:13 +0000

    add post endpoint for migration

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,14 +1,21 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path"
+import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  if(request.method !== 'GET' && request.method !== 'POST') {
+    return response.status(405).end();
+  }
   const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
+    dryRun: getMethod(request.method),
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
-    migrationsTable: "pgmigrations"
+    migrationsTable: "pgmigrations",
   });
   response.status(200).json(migrations);
+}
+
+function getMethod(method) {
+  return method === "GET" ? true : false;
 }
--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -1,5 +1,5 @@
 test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations/migrations");
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
