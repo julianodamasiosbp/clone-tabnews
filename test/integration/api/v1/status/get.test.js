4b1a227f3e0329826b83400e80565aaaddcab33f
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-01-13 20:21:40 +0000

    test: rename all descriptions to use new pattern

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -4,24 +4,30 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const ambiente = process.env.NODE_ENV === "production" ? "prod" : "dev";
-  const postgresVersion = ambiente === "dev" ? "16.0" : "16.6";
-  const maxConnections = ambiente === "dev" ? 100 : 112;
+describe("GET /api/v1/status", () => {
+  describe("Anonynous user", () => {
+    test("Retrieving current system status", async () => {
+      const ambiente = process.env.NODE_ENV === "production" ? "prod" : "dev";
+      const postgresVersion = ambiente === "dev" ? "16.0" : "16.6";
+      const maxConnections = ambiente === "dev" ? 100 : 112;
 
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      const responseBody = await response.json();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual(postgresVersion);
+      expect(responseBody.dependencies.database.version).toEqual(
+        postgresVersion,
+      );
 
-  expect(responseBody.dependencies.database.max_connections).toEqual(
-    maxConnections,
-  );
+      expect(responseBody.dependencies.database.max_connections).toEqual(
+        maxConnections,
+      );
 
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
