1e32f95a21e563d7c57ad38207b0eaf9ddb34878
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-06-14 14:11:25 +0000

    feat: add user migration, add `runPendingMigrations` method to `orchestrator`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,6 @@
 import orchestrator from "tests/orchestrator.js";
 import { version as uuidVersion } from "uuid";
+import database from "infra/database.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -114,6 +115,28 @@ describe("POST /api/v1/users", () => {
         action: "Utilize outro usuário ou email para realizar o cadastro.",
         status_code: 400,
       });
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["pedrosilva", "pedrosilva@email.com", "senha123"],
+      });
+
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["pedrosilva2", "Pedrosilva@email.com", "123senha"],
+      });
+
+      const users = await database.query("SELECT * FROM users;");
+      console.log(users.rows);
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+      expect(Array.isArray(responseBody)).toBe(true);
+
+      expect(responseBody.length).toBeGreaterThanOrEqual(1);
+      expect(responseBody[0].path.includes("infra/migrations")).toBe(true);
     });
   });
 });
