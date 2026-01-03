527d13cd9f9450d37e29725e07e1272753fed8e7
Juliano Damasio <julianodamasiosbp@gmail.com>
2026-01-03 15:14:58 +0000

    fix: change package.json build script

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -7,10 +7,7 @@ const router = createRouter();
 router.get(getHandler);
 router.post(postHandler);
 
-export default router.handler({
-  onNoMatch: controller.onNoMatchHandler,
-  onError: controller.onErrorHandler,
-});
+export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const pendingMigrations = await migrator.listPendingMigrations();
